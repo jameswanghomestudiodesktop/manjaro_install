b8eaead8a5c658ae9591936c696b7faee25caf17
James <james_register@163.com>
2020-12-27 21:46:59 +0800

    add git commands alias

--- install_temp/initial_install.sh
+++ install_temp/initial_install.sh
@@ -1,6 +1,13 @@
-yay -S base-devel vim
+yay -S base-devel vim net-tools
 sudo cp blacklist-nouveau.conf /etc/modprobe.d/
 
 sudo pacman -Rsn xf86-video-nouveau
 reboot
 sudo systemctl enable sshd
+
+
+
+# data base initialize
+
+sudo mysql_install_db --basedir=/usr --datadir=/home/james/Storage/mariadb_data --user=mysql --ldata=/var/lib/mysql
+
