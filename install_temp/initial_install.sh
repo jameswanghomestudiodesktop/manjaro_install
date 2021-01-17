d251755e1805066df69ccdb7b5ddafb57007c64a
James Wang <james_register@163.com>
2021-01-17 12:27:36 +0800

    added ubuntu xfce

--- install_temp/initial_install.sh
+++ install_temp/initial_install.sh
@@ -10,4 +10,6 @@ sudo systemctl enable sshd
 # data base initialize
 
 sudo mysql_install_db --basedir=/usr --datadir=/home/james/Storage/mariadb_data --user=mysql --ldata=/var/lib/mysql
+# ubuntu xfce
+sudo mysql_install_db --datadir=/Storage/mariadb_data  --basedir=/usr --user=mysql --ldata=/Storage/mariadb_data/
 
